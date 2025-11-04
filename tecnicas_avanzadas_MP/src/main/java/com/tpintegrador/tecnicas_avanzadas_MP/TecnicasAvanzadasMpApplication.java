e7168f7116d999931c3ad17f2876ffce3a725eab
Martin Pardo <martupardo@hotmail.com>
2025-11-04 14:11:06 -0300

    Initialize Spring App

---
+++ tecnicas_avanzadas_MP/src/main/java/com/tpintegrador/tecnicas_avanzadas_MP/TecnicasAvanzadasMpApplication.java
@@ -0,0 +1,13 @@
+package com.tpintegrador.tecnicas_avanzadas_MP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnicasAvanzadasMpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnicasAvanzadasMpApplication.class, args);
+	}
+
+}
---
+++ tecnicas_avanzadas_MP/src/test/java/com/tpintegrador/tecnicas_avanzadas_MP/TecnicasAvanzadasMpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpintegrador.tecnicas_avanzadas_MP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnicasAvanzadasMpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
